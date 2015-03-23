c8a9fa2cfbdcf787e91474bd9e9cc264dac1af85
Andy Gaskell <andygaskelluk@yahoo.co.uk>
2015-03-23 19:34:50 +0000

    Update ssofbcolumns.php
    latest version

--- ssofbcolumns/ssofbcolumns.php
+++ ssofbcolumns/ssofbcolumns.php
@@ -19,7 +19,18 @@ blah blah
 {colbr}
 blah blah 
 {/cols} 
- 
+
+Secondary Syntax:
+blah blah 
+{split}
+blah blah 
+  
+Tertiary Syntax:
+blah blah 
+{splitleft}
+blah blah   
+  
+  
 */
  
  // no direct access
@@ -36,6 +47,12 @@ class plgContentSsofbcolumns extends JPlugin {
         $num_col_open = substr_count ($article->text, "{cols}");
         $num_col_br = substr_count ($article->text, "{colbr}");
         $num_col_close = substr_count ($article->text, "{/cols}");
+
+        # see if we have a split
+        $split = substr_count ($article->text, "{split}");        
+
+        # see if we have a split left
+        $split_left = substr_count ($article->text, "{splitleft}");        
         
         #$notices .= "num_col_open: " . $num_col_open . " <br/>";        
         #$notices .= "num_col_br: " . $num_col_br . " <br/>";        
@@ -48,17 +65,22 @@ class plgContentSsofbcolumns extends JPlugin {
                 # we have 2 cols, so span6
                 $span = "span6";
             } elseif ( $num_col_br == 2 ) {
-                # we have 3 cols, so span6
+                # we have 3 cols, so span4
                 $span = "span4";
             } elseif ( $num_col_br == 3 ) {
-                # we have 4 cols, so span6
+                # we have 4 cols, so span3
                 $span = "span3";
+            } elseif ( $num_col_br == 4 ) {
+                # we have 5 cols, so span5Col, this is a bit of a special
+                $span = "span5Col";    
             } elseif ( $num_col_br == 5 ) {
                 # we have 6 cols, so span2
-                $span = "span6";
+                $span = "span2";
             } elseif ( $num_col_br == 11 ) {
                 # we have 12 cols, so span1
-                $span = "span6";
+                $span = "span1";
+                
+                
             } else {
                 # we have an invalid number of cols
                 $span = "";
@@ -77,8 +99,27 @@ class plgContentSsofbcolumns extends JPlugin {
             # if there are any notices, then lets show them at the top of the article
             if ($notices) {
                 $article->text = "<div class=\"alert\">" . $notices . "</div>" . $article->text;
-            }        
+            }       
+
+            
+        } elseif ($split) { 
+        
+            # sub in the div close and open
+            $article->text = str_replace('{split}', '</div><div class="span4">', $article->text);
+            # add a div to front and back
+            $article->text = "<div class=\"row-fluid\"><div class=\"span8\">" . $article->text . "</div></div>";
+
+            
+        } elseif ($split_left) { 
+
+            # sub in the div close and open
+            $article->text = str_replace('{splitleft}', '</div><div class="span8">', $article->text);
+            # add a div to front and back
+            $article->text = "<div class=\"row-fluid\"><div class=\"span4\">" . $article->text . "</div></div>";
+
+        
         } else {
+        
             return true;
         }
     }
